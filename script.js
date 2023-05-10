819b3eb07569eb04aa5667859a8b5649d5287297
omar-mFarrag <omar.mustafa.ff@gmail.com>
2023-05-10 20:01:54 +0200

    add dots

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let hours = document.getElementById('hh');
 let minutes = document.getElementById('mm');
 let seconds = document.getElementById('ss');
 
+let hoursDot = document.getElementById('hhdot');
+let minutesDot = document.getElementById('mmdot');
+let secondsDot = document.getElementById('ssdot');
+
 setInterval(() => {
     var hh = new Date().getHours();
     var mm = new Date().getMinutes();
@@ -14,4 +18,8 @@ setInterval(() => {
     hours.style.transform = `rotate( calc( ${hh + (mm / 60)} * 30deg ) )`;
     minutes.style.transform = `rotate( calc( ${mm + (ss / 60)} * 6deg ) )`;
     seconds.style.transform = `rotate( calc( ${ss} * 6deg ) )`;
+
+    hoursDot.style.transform = `rotate( calc( ${hh + (mm / 60)} * 30deg ) )`;
+    minutesDot.style.transform = `rotate( calc( ${mm + (ss / 60)} * 6deg ) )`;
+    secondsDot.style.transform = `rotate( calc( ${ss} * 6deg ) )`;
 }, 1000);
